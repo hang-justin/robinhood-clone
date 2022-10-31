b2d85f3b225403d75e1f8cff74bb7c62a270fff9
Justin <hangjustin@gmail.com>
2022-10-31 00:24:25 -0700

    Watchlist styling

--- react-app/src/components/UserHomePage/UserHomeSideBar/Watchlist.js
+++ react-app/src/components/UserHomePage/UserHomeSideBar/Watchlist.js
@@ -3,27 +3,32 @@ import { useSelector } from "react-redux"
 import SidebarRow from "./SidebarRow"
 
 import './Watchlist.css'
+import arrow from '../../img/up-arrow.png'
 
 const Watchlist = ({ watchlist }) => {
     const [showWatchlistItems, setShowWatchlistItems] = useState(false)
+    const [watchlistStatus, setWatchlistStatus] = useState(false)
     const ownedAssets = useSelector(state => state.assets)
 
     const watchlistItemsArr = watchlist.items
 
     const toggleItemDisplay = () => {
         setShowWatchlistItems(prev => !prev)
+        setWatchlistStatus((prev) => !prev)
     }
 
     return (
         <>
             <div className='user-watchlist sidebar-row-content flx-row justify-space-btw' onClick={toggleItemDisplay}>
 
-                <div className='watchlist-name'>
-                    {watchlist.name}
+                <div className='watchlist-name-header'>
+                    <span className='watchlist-emoji'>💡</span> <span className='watchlist-name'>{watchlist.name}</span>
                 </div>
 
                 <div className='watchlist-buttons'>
-                    expand/collapse
+                    <span class={`material-symbols-outlined ${watchlistStatus ? 'collapsed' : 'expanded'}`}>
+                        arrow_forward_ios
+                    </span>
                 </div>
             </div>
 
