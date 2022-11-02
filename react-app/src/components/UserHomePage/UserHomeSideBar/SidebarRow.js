9f9b6c55f802af622af0ef82ac0303a9f41d86c9
Justin <hangjustin@gmail.com>
2022-11-02 15:33:24 -0700

    Sidebar currency and percentage CSS

--- react-app/src/components/UserHomePage/UserHomeSideBar/SidebarRow.js
+++ react-app/src/components/UserHomePage/UserHomeSideBar/SidebarRow.js
@@ -1,9 +1,20 @@
 
+import { useSelector } from 'react-redux'
+import formatMoney from '../../../util/formatMoney'
 import sparkline from '../../img/sparkline.PNG'
 import './SidebarRow.css'
 
 const SidebarRow = ({ asset }) => {
-    
+    const latestPrices = useSelector(state => state.market.allLatest)
+
+    const price = latestPrices[asset.asset_id].usd > 1 ?
+                    formatMoney(latestPrices[asset.asset_id].usd) :
+                    '$' + latestPrices[asset.asset_id].usd.toString()
+
+    const dailyChangePercentage = latestPrices[asset.asset_id].usd_24h_change.toFixed(2)
+
+    const percentageChangeTextColor = dailyChangePercentage >= 0 ? 'main-green-text' : 'main-red-text';
+
     return (
         <div className='sidebar-row  sidebar-row-content flx-row-align-ctr justify-space-btw'>
 
@@ -15,8 +26,8 @@ const SidebarRow = ({ asset }) => {
             <img className='sparkline' src={sparkline} />
 
             <div className='sidebar-row__right flx-col-align-ctr'>
-                <span>$100.00</span>
-                <span>+100%</span>
+                <span className='margin-left-auto'>{price}</span>
+                <span className={`margin-left-auto ${percentageChangeTextColor}`}>{dailyChangePercentage}%</span>
             </div>
 
         </div>
