05b1661ff619d10c7940908bba872a9b5311d137
Justin <hangjustin@gmail.com>
2022-11-06 01:52:47 -0800

    Added socials

--- react-app/src/components/SplashPage/GuestNavBar/index.js
+++ react-app/src/components/SplashPage/GuestNavBar/index.js
@@ -3,6 +3,8 @@ import './GuestNavBar.css';
 
 import logo from '../../img/logo.png'
 import logoInverted from '../../img/logo-inverted.png'
+import github from '../../img/github.png'
+import linkedin from '../../img/linkedin.png'
 
 
 const GuestNavBar = () => {
@@ -15,9 +17,21 @@ const GuestNavBar = () => {
                     Yuanhood
                 </div>
 
-                <div id='splash-logo-container'>
+                <div id='splash-logo-container' className='flx-row-align-ctr justify-space-btw'>
                     <img id='splash-logo' src={logo} alt='yuanhood-logo'/>
                 </div>
+
+                <div id='social-links' className='flx-row-justify-align-ctr'>
+                    <a className='link-to-social flx-row-align-ctr' href='https://github.com/hang-justin'>
+                        <img id='github-logo' className='social-logo-img' src={github} alt='github-logo-link'/>
+                    </a>
+
+                    <a className='link-to-social flx-row-align-ctr' href='https://www.linkedin.com/in/hang-justin/'>
+                        <img id='linkedin-logo' className='social-logo-img' src={linkedin} alt='linkedin-logo-link'/>
+                    </a>
+
+                </div>
+
             </div>
 
             <div id='guest-session-btn-container' className='flx-row-justify-align-ctr'>
